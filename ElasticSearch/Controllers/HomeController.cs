349cb280acc321082b3e8d3694e5cefbfac9f407
Batuhan FINDIK <batuhanf@med-case.io>
2023-02-21 21:41:45 +0300

    Add project files.

---
+++ ElasticSearch/Controllers/HomeController.cs
@@ -0,0 +1,188 @@
+﻿using ElasticSearch.Models;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Nest;
+using System.Diagnostics;
+
+namespace ElasticSearch.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ConnectionSettings connSettings;
+
+        private readonly ElasticClient elasticClient;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            connSettings = new ConnectionSettings(new Uri("http://localhost:9200/")).BasicAuthentication("elastic", "t82KZDcP8tDtQsSfRG1R")
+           .DefaultIndex("urunler").DefaultMappingFor<Urun>(m => m.IndexName("urunler"));
+            elasticClient = new ElasticClient(connSettings);
+        }
+
+        public IActionResult Index()
+        {
+            try
+            {
+                Indexing();
+                return View(UrunBul());
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+          
+        }
+        private void Indexing()
+        {
+            List<Urun> urunler = new List<Urun>();
+            Urun urun1 = new Urun()
+            {
+                Id = "1",
+                Ad = "Samsung_A50",
+                Aciklama = "Samsung A50 yeni model",
+                Fiyat = "10.000TL",
+                Fotograf = "https://cdn.shopify.com/s/files/1/0289/5790/0859/products/samsung-a50-screen-repair-funtech-repair-sama50lcd.jpg?v=1591806465"
+            };
+            Urun urun2 = new Urun()
+            {
+                Id = "2",
+                Ad = "Iphone 13",
+                Aciklama = "Iphone 13 yeni model",
+                Fiyat = "30.000TL",
+                Fotograf = "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-13-finish-select-202207-6-1inch-blue?wid=2560&hei=1440&fmt=jpeg&qlt=95&.v=1656712888128"
+            };
+            Urun urun3 = new Urun()
+            {
+                Id = "3",
+                Ad = "Huawei Nova",
+                Aciklama = "Huawei Nova 10 yeni model",
+                Fiyat = "20.000TL",
+                Fotograf = "https://img01.huaweifile.com/eu/tr/huawei/uomcdn/TRHW/pms/202209/gbom/6941487272891/428_428_6FF944BCA74385C3D7FE356E84A0FC22mp.png"
+            };
+            Urun urun4 = new Urun()
+            {
+                Id = "4",
+                Ad = "Samsung A52",
+                Aciklama = "Samsung A52 yeni model",
+                Fiyat = "15.000TL",
+                Fotograf = "https://cdn.shopify.com/s/files/1/0289/5790/0859/products/samsung-a50-screen-repair-funtech-repair-sama50lcd.jpg?v=1591806465"
+            };
+            urunler.Add(urun1);
+            urunler.Add(urun2);
+            urunler.Add(urun3);
+            urunler.Add(urun4);
+
+            var defaultIndex = "urunler";
+            var client = new ElasticClient();
+
+            if (elasticClient.Indices.Exists(defaultIndex).Exists)
+            {
+                client.Indices.Delete(defaultIndex);
+            }
+
+            if (!elasticClient.Indices.Exists("urun_alias").Exists)
+            {
+                client.Indices.Create(defaultIndex, c => c
+                    .Mappings(m => m
+                        .Map<Urun>(mm => mm
+                            .AutoMap()
+                        )
+                    ).Aliases(a => a.Alias("urun_alias"))
+                );
+            }
+
+            // Bulk Insert
+            var bulkIndexer = new BulkDescriptor();
+
+            foreach (var document in urunler)
+            {
+                bulkIndexer.Index<Urun>(i => i
+                    .Document(document)
+                    .Id(document.Id)
+                    .Index("urunler"));
+            }
+
+            elasticClient.Bulk(bulkIndexer);
+
+        }
+        public List<Urun> UrunBul(string value = "")
+        {
+            try
+            {
+
+                //var urunler = elasticClient.Search<Urun>(s => s
+                //  .Index("urunler")
+                //  .Query(q => q.MatchPhrase(m => m
+                //      .Field(f => f.Ad)
+                //      .Query(value)))).Documents.ToList();
+
+                //var urunler = elasticClient.Search<Urun>
+                // (s => s
+                // .Query(query => query.Match(x => x.(x => x.Ad).Value(value)))).Documents.ToList();
+
+                //var urunler = elasticClient.Search<Urun>
+                // (s => s
+                // .Query(query => query.Match(b => b.Name("*"+value+"*")
+                // ))).Documents.ToList();
+                var urunler = elasticClient.Search<Urun>
+                (s => s.From(0)
+                .Query(query => query.Bool(b => b.Must(
+                    x => x.Wildcard(x => x.Field("ad").Value("*" + value + "*"))
+                )))).Documents.ToList();
+
+                //var urunler = elasticClient.Search<Urun>
+                // (s => s.From(0)
+                // .Query(query => query.Bool(b => b.Must(
+                //     x => x.Wildcard(x => x.Field("ad").Value("*" + value.Replace(" ","_") + "*"))
+                // )))).Documents.ToList();
+
+                // var urunler = elasticClient.Search<Urun>
+                //(s => s.From(0).Query(query => query.QueryString(x=>x.Fields(x=>x.Field(x=>x.Ad)).Query("*"+value+" *"))
+
+                //)
+                //).Documents.ToList();
+
+                //match
+                //if (value != "")
+                //{
+                //    var veri = value.Replace(@" ", @"\").Substring(0, 8);
+                //}
+
+                //   var urunler = elasticClient.Search<Urun>
+                // (s => s.From(0)
+                // .Query(query => query.Bool(b => b.Must(
+                //      sd => sd.MatchPhrase(m => m
+                //    .Field(f => f.Ad)
+                //    .Query(value)
+                //    ),
+                //sd => sd.Terms(m => m
+                //    .Field(f => f.Ad)
+                //    .Terms<string>(" *" + value + "*")
+                //    )
+                // )))).Documents.ToList();
+
+                //    matchall
+
+                return urunler;
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ElasticSearch/Models/Urun.cs
@@ -0,0 +1,11 @@
+﻿namespace ElasticSearch.Models
+{
+    public class Urun
+    {
+        public string Id { get; set; }
+        public string Ad { get; set; }
+        public string Fotograf { get; set; }
+        public string Aciklama { get; set; }
+        public string Fiyat { get; set; }
+    }
+}
